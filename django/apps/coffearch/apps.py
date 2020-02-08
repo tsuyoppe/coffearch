a8304e28e1b12d514b027cecb7acd280f0598415
keii1111 <kexesong1995@icloud.com>
2020-02-08 18:59:46 +0900

    remove comment

--- django/apps/coffearch/apps.py
+++ django/apps/coffearch/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class CoffearchConfig(AppConfig):
     name = 'apps.coffearch'
--- django/apps/coffearch/models.py
+++ django/apps/coffearch/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Country(models.Model):
     name = models.CharField(max_length=255)
     continent = models.CharField(max_length=50)
