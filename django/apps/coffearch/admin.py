b68ebc184baf2ee2672ace7be05acee123d8a6a0
keii1111 <kexesong1995@icloud.com>
2020-02-09 13:32:32 +0900

    Add all Models to admin site.

--- django/apps/coffearch/admin.py
+++ django/apps/coffearch/admin.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
 
+from .models import Country, Region, Coffee, CoffeeFlavor
+
+admin.site.register(Country)
+admin.site.register(Region)
+admin.site.register(Coffee)
+admin.site.register(CoffeeFlavor)
