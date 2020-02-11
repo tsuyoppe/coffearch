28df703457f7a97d624701a817e84f4fbe3e1a4b
keii1111 <kexesong1995@icloud.com>
2020-02-11 16:31:20 +0900

    Add country detail page, which is stab.

--- django/apps/coffearch/views.py
+++ django/apps/coffearch/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Country
+
 def index(request):
-    return HttpResponse("Hello, here is coffearch index page!!")
+    country_list = Country.objects.order_by('continent')
+    context = {
+        'countries': [
+            {'name': country.name }
+            for country in country_list
+            ]
+    }
+    return render(request, 'coffearch/index.html', context)
+
+def detail(request, country_name):
+    return HttpResponse(f'this page is {country_name} detail page!!')
