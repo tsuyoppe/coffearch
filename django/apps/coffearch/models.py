4371a05640ad47b7cf4135ef6fb9d6023c5794c5
keii1111 <kexesong1995@icloud.com>
2020-02-11 14:39:43 +0900

    Change models and add new model, Flavor.

--- django/apps/coffearch/models.py
+++ django/apps/coffearch/models.py
@@ -4,16 +4,20 @@ class Country(models.Model):
     name = models.CharField(max_length=255)
     continent = models.CharField(max_length=50)
     coffee_production = models.IntegerField(default=0)
+    description = models.TextField(null=True)
 
     def __str__(self):
         return self.name
+
 
 class Region(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
 
     def __str__(self):
         return self.name
+
 
 class Coffee(models.Model):
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
@@ -21,7 +25,24 @@ class Coffee(models.Model):
     body = models.FloatField()
     acidity = models.FloatField()
     bitterness = models.FloatField()
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Flavor(models.Model):
+    name = models.CharField(max_length=255)
+    first_type = models.CharField(max_length=255)
+    second_type = models.CharField(max_length=255, null=True)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
 
 class CoffeeFlavor(models.Model):
     coffee = models.ForeignKey(Coffee, on_delete=models.CASCADE)
-    flavor = models.CharField(max_length=255)
+    flavor = models.ForeignKey(Flavor, on_delete=models.CASCADE)
+    description = models.TextField(null=True)
+
